81ae23f75f7f54355fb1ea9a9a18ccb5f5cb2c9e
Alan <langwdalan@gmail.com>
2022-12-17 12:00:23 +0800

    feat: Go to selection mode after adding a shape (#8)

--- src/view/toolbar.rs
+++ src/view/toolbar.rs
@@ -4,70 +4,63 @@ use crate::{store::AppState, widget::WidgetKind};
 
 #[component]
 pub fn Toolbar<G: Html>(ctx: Scope) -> View<G> {
-    let app_state = use_context::<AppState>(ctx);
-    let value = *app_state.selected_kind.get();
-
-    let on_selected = |kind: WidgetKind| {
-        tracing::info!("kind down at ({:?})", kind);
-        app_state.set_selected_kind(kind);
-    };
     // TODO：使用迭代器来减少下面的重复代码
     view! (ctx,
         div {
             ElementOption(
                 text=WidgetKind::Rectangle.to_string(),
                 kind=WidgetKind::Rectangle,
-                on_click= Box::new(on_selected),
-                checked = value == WidgetKind::Rectangle
             )
             ElementOption(
                 text=WidgetKind::Ellipse.to_string(),
                 kind=WidgetKind::Ellipse,
-                on_click= Box::new(on_selected),
-                checked = value == WidgetKind::Ellipse
             )
             ElementOption(
                 text=WidgetKind::Arrow.to_string(),
                 kind=WidgetKind::Arrow,
-                on_click= Box::new(on_selected),
-                checked = value == WidgetKind::Arrow
             )
             ElementOption(
                 text=WidgetKind::Text.to_string(),
                 kind=WidgetKind::Text,
-                on_click= Box::new(on_selected),
-                checked = value == WidgetKind::Text
             )
             ElementOption(
                 text=WidgetKind::Selection.to_string(),
                 kind=WidgetKind::Selection,
-                on_click= Box::new(on_selected),
-                checked = value == WidgetKind::Selection
             )
         }
     )
 }
 
 #[derive(Prop)]
-struct ElementOptionProps<'a> {
-    checked: bool,
+struct ElementOptionProps {
     text: String,
     kind: WidgetKind,
-    on_click: Box<dyn Fn(WidgetKind) + 'a>,
 }
 
 #[component]
-fn ElementOption<'a, G: Html>(ctx: Scope<'a>, props: ElementOptionProps<'a>) -> View<G> {
-    let on_click = props.on_click;
+fn ElementOption<G: Html>(ctx: Scope, props: ElementOptionProps) -> View<G> {
+    let app_state = use_context::<AppState>(ctx);
+
+    let on_selected = |kind: WidgetKind| {
+        tracing::info!("kind down at ({:?})", kind);
+        app_state.set_selected_kind(kind);
+        app_state.clear_selection_elements();
+    };
+
+    let checked = create_signal(ctx, *app_state.selected_kind.get() == props.kind);
+    create_effect(ctx, move || {
+        checked.set(*app_state.selected_kind.get() == props.kind)
+    });
+
     view! (ctx,
         label(class="cursor-pointer") {
             input(
                 class="cursor-pointer",
                 type="radio",
                 name="element",
-                value="circle",
-                on:click=move |_| on_click(props.kind),
-                checked=props.checked
+                on:click=move |_| on_selected(props.kind),
+                value=checked,
+                bind:checked=checked
             )
             (props.text)
         }
